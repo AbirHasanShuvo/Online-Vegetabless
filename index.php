211209ccd8c4b76db3b6c4709766f6455d58d0d8
AbirHasanShuvo <abirhasanshuvo789@gmail.com>
2023-06-03 16:56:55 +0600

    First Commit

---
+++ index.php
@@ -0,0 +1,279 @@
+<?php include("header.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+
+
+
+<div class="container mt-5">
+        <div class="row">
+<div class="col-lg-3">
+    <form action="manage_cart.php" method = "POST">
+  <div class="card">
+     <img src="images/cabbage.png" class="card-img-top">
+    <div class="card-body text-center">
+    <h5 class="card-title">Cabbage</h5>
+    <p class="card-text">Price : 70৳</p>
+    <button type = "Submit" name = "ADD_TO_CART" class="btn btn-info">Add To Cart</button>
+    <input type = "hidden" name = "Item_Name" value = "Cabbage">
+    <input type = "hidden" name = "Price" value = "70">
+
+  </div>
+  
+</div>
+     </form>
+
+
+            </div>
+
+            <div class="col-lg-3">
+    <form action="manage_cart.php" method = "POST">
+  <div class="card">
+     <img src="images/brinjal.png" class="card-img-top">
+    <div class="card-body text-center">
+    <h5 class="card-title">Brinjal</h5>
+    <p class="card-text">Price : 190৳</p>
+    <button type = "Submit" name = "ADD_TO_CART" class="btn btn-info">Add To Cart</button>
+    <input type = "hidden" name = "Item_Name" value = "Brinjal">
+    <input type = "hidden" name = "Price" value = "190">
+
+  </div>
+  
+</div>
+     </form>
+
+
+            </div> 
+
+
+            <div class="col-lg-3">
+    <form action="manage_cart.php" method = "POST">
+  <div class="card">
+     <img src="images/pumpkin.png" class="card-img-top">
+    <div class="card-body text-center">
+    <h5 class="card-title">Pumpkin</h5>
+    <p class="card-text">Price : 190৳</p>
+    <button type = "Submit" name = "ADD_TO_CART" class="btn btn-info">Add To Cart</button>
+    <input type = "hidden" name = "Item_Name" value = "Pumpkin">
+    <input type = "hidden" name = "Price" value = "190">
+
+  </div>
+  
+</div>
+     </form>
+
+
+            </div> 
+
+            <div class="col-lg-3">
+    <form action="manage_cart.php" method = "POST">
+  <div class="card">
+     <img src="images/bean.png" class="card-img-top">
+    <div class="card-body text-center">
+    <h5 class="card-title">Bean</h5>
+    <p class="card-text">Price : 160৳</p>
+    <button type = "Submit" name = "ADD_TO_CART" class="btn btn-info">Add To Cart</button>
+    <input type = "hidden" name = "Item_Name" value = "Bean">
+    <input type = "hidden" name = "Price" value = "160">
+
+  </div>
+  
+</div>
+     </form>
+
+
+            </div> 
+
+            <div class="col-lg-3">
+    <form action="manage_cart.php" method = "POST">
+  <div class="card">
+     <img src="images/bitter_gourd.png" class="card-img-top">
+    <div class="card-body text-center">
+    <h5 class="card-title">Bitter Gourd</h5>
+    <p class="card-text">Price : 90৳</p>
+    <button type = "Submit" name = "ADD_TO_CART" class="btn btn-info">Add To Cart</button>
+    <input type = "hidden" name = "Item_Name" value = "Bitter Gourd">
+    <input type = "hidden" name = "Price" value = "90">
+
+  </div>
+  
+</div>
+     </form>
+
+
+            </div> 
+
+            <div class="col-lg-3">
+    <form action="manage_cart.php" method = "POST">
+  <div class="card">
+     <img src="images/cucumber.png" class="card-img-top">
+    <div class="card-body text-center">
+    <h5 class="card-title">Cucumber</h5>
+    <p class="card-text">Price : 80৳</p>
+    <button type = "Submit" name = "ADD_TO_CART" class="btn btn-info">Add To Cart</button>
+    <input type = "hidden" name = "Item_Name" value = "Cucumber">
+    <input type = "hidden" name = "Price" value = "80">
+
+  </div>
+  
+</div>
+     </form>
+
+
+            </div> 
+
+            <div class="col-lg-3">
+    <form action="manage_cart.php" method = "POST">
+  <div class="card">
+     <img src="images/gourd.png" class="card-img-top">
+    <div class="card-body text-center">
+    <h5 class="card-title">Gourd</h5>
+    <p class="card-text">Price : 130৳</p>
+    <button type = "Submit" name = "ADD_TO_CART" class="btn btn-info">Add To Cart</button>
+    <input type = "hidden" name = "Item_Name" value = "Gourd">
+    <input type = "hidden" name = "Price" value = "130">
+
+  </div>
+  
+</div>
+     </form>
+
+
+            </div> 
+
+            <div class="col-lg-3">
+    <form action="manage_cart.php" method = "POST">
+  <div class="card">
+     <img src="images/green_banana.png" class="card-img-top">
+    <div class="card-body text-center">
+    <h5 class="card-title">Green Banana</h5>
+    <p class="card-text">Price : 140৳</p>
+    <button type = "Submit" name = "ADD_TO_CART" class="btn btn-info">Add To Cart</button>
+    <input type = "hidden" name = "Item_Name" value = "Green Banana">
+    <input type = "hidden" name = "Price" value = "140">
+
+  </div>
+  
+</div>
+     </form>
+
+
+            </div> 
+
+            <div class="col-lg-3">
+    <form action="manage_cart.php" method = "POST">
+  <div class="card">
+     <img src="images/green_papaya.png" class="card-img-top">
+    <div class="card-body text-center">
+    <h5 class="card-title">Green Papaya</h5>
+    <p class="card-text">Price : 180৳</p>
+    <button type = "Submit" name = "ADD_TO_CART" class="btn btn-info">Add To Cart</button>
+    <input type = "hidden" name = "Item_Name" value = "Green Papaya">
+    <input type = "hidden" name = "Price" value = "180">
+
+  </div>
+  
+</div>
+     </form>
+
+
+            </div> 
+
+            <div class="col-lg-3">
+    <form action="manage_cart.php" method = "POST">
+  <div class="card">
+     <img src="images/ladies_finger.png" class="card-img-top">
+    <div class="card-body text-center">
+    <h5 class="card-title">Ladies Finger</h5>
+    <p class="card-text">Price : 95৳</p>
+    <button type = "Submit" name = "ADD_TO_CART" class="btn btn-info">Add To Cart</button>
+    <input type = "hidden" name = "Item_Name" value = "Ladies Finger">
+    <input type = "hidden" name = "Price" value = "95">
+
+  </div>
+  
+</div>
+     </form>
+
+
+            </div> 
+
+            <div class="col-lg-3">
+    <form action="manage_cart.php" method = "POST">
+  <div class="card">
+     <img src="images/raddish.png" class="card-img-top">
+    <div class="card-body text-center">
+    <h5 class="card-title">Raddish</h5>
+    <p class="card-text">Price : 130৳</p>
+    <button type = "Submit" name = "ADD_TO_CART" class="btn btn-info">Add To Cart</button>
+    <input type = "hidden" name = "Item_Name" value = "Raddish">
+    <input type = "hidden" name = "Price" value = "130">
+
+  </div>
+  
+</div>
+     </form>
+
+
+            </div> 
+
+            <div class="col-lg-3">
+    <form action="manage_cart.php" method = "POST">
+  <div class="card">
+     <img src="images/Sponge_Gourd.png" class="card-img-top">
+    <div class="card-body text-center">
+    <h5 class="card-title">Sponge Gourd</h5>
+    <p class="card-text">Price : 210৳</p>
+    <button type = "Submit" name = "ADD_TO_CART" class="btn btn-info">Add To Cart</button>
+    <input type = "hidden" name = "Item_Name" value = "Sponge Gourd">
+    <input type = "hidden" name = "Price" value = "210">
+
+  </div>
+  
+</div>
+     </form>
+
+
+            </div> 
+
+            
+
+           
+
+
+            <div class="col-lg-3">
+    <form action="manage_cart.php" method = "POST">
+  <div class="card">
+     <img src="images/carrot.png" class="card-img-top">
+    <div class="card-body text-center">
+    <h5 class="card-title">Carrot</h5>
+    <p class="card-text">Price : 170৳</p>
+    <button type = "Submit" name = "ADD_TO_CART" class="btn btn-info">Add To Cart</button>
+    <input type = "hidden" name = "Item_Name" value = "Carrot">
+    <input type = "hidden" name = "Price" value = "170">
+
+  </div>
+  
+</div>
+     </form>
+
+
+            </div> 
+        </div>
+    </div>
+
+
+  
+
+
+
+
+</body>
+</html>
